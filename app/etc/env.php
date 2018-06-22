e4ab40b6c1a8fc03f542898c33e295826d5e8e07
Daniel <daniel@accolade.es>
2018-06-22 10:12:30 +0100

    INIT

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'a49f3115c41426bd6042b94c0f3469d1',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'root',
+        'password' => 'nvidias',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Fri, 22 Jun 2018 09:00:45 +0000',
+  ),
+);
---
+++ pub/static/adminhtml/Magento/backend/en_US/es6-collections.js
@@ -0,0 +1,1 @@
+/var/www/html/magento2/magento2/lib/web/es6-collections.js
